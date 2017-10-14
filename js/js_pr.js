4d912662fbb643d49b559f8d2900ad631fdeafb0
lizhenjiesid <32793484+lizhenjiesid@users.noreply.github.com>
2017-10-14 19:12:27 +0800

    inital commit

---
+++ js/js_pr.js
@@ -0,0 +1,29 @@
+// JavaScript Document
+var intput ;
+function fajax(){
+"use strict";	
+$.ajax({ 
+    type: 'GET', 
+    url: 'https://jsonplaceholder.typicode.com/posts?userId=' + document.getElementById("userinput").value, 
+
+    dataType: 'json',
+    success: function (data) { 
+		
+        $.each(data, function(index, element) {
+            //$("#jsontTable").append($("<p>").html(element.id) );
+			
+			var divRow = $("<div>").addClass("divTableRow");
+			var divCellId = $("<div>").addClass("divTableCell").html(element.id);
+			var DivCellBody = $("<div>").addClass("divTableCell").html(element.body);
+			var divCellTitle = $("<div>").addClass("divTableCell").html(element.title);
+			divRow.append(divCellId,divCellTitle,DivCellBody);
+			$(".divTableBody").append(divRow);
+            });
+       }
+   });
+}
+function myrefresh()
+ {    "use strict";	
+      window.location.reload();
+ }
+
